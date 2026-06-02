923c819c9097be8925ce520502f597b5c1d6d619
Harshavardhan Ravichander <harshavardhan.r.misc@gmail.com>
2026-06-02 08:39:30 +0000

    Drawing a Circle in SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
-#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
 using namespace sf;
 
-int main()
-{
-	Window window(VideoMode({800, 600}), "Window fr");
+int main() {
 
-	while(window.isOpen()){
-		while(const std::optional event = window.pollEvent()){
-			if(event->is<Event::Closed>())
+	RenderWindow window(VideoMode(), "Drawing a circle", State::Fullscreen);
+	float height = window.getSize().x;
+	float width = window.getSize().y;
+
+	CircleShape circle(100.f);
+	circle.setOrigin({100.f, 100.f});
+	circle.setPosition({height/2, width/2});
+	circle.setFillColor(Color::Blue);
+
+	while(window.isOpen()) {
+
+		while(const std::optional event = window.pollEvent()) {
+			if(event->is<Event::Closed>()) 
 				window.close();
+			else if(const auto* keyPressed = event->getIf<Event::KeyPressed>()) {
+				if(keyPressed->scancode == Keyboard::Scancode::Escape) 
+					window.close();
+			}
 		}
+
+		window.clear(Color::Black);
+		window.draw(circle); //draw stuff
+		window.display();
+
 	}
+
+	std::cout << "Window closed!" << std::endl;
+
 }
